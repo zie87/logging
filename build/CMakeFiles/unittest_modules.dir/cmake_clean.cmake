10824c2ac1fb18e7c4dc9ae400cc32aec32199e3
zie87 <tobias.zindl@googlemail.com>
2015-07-29 15:08:58 +0200

    removed temp-files and  added gitignor

--- build/CMakeFiles/unittest_modules.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/unittest_modules"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/unittest_modules.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/tests/unit_tests/factory_blog/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/IDMT-WORKSPACE/DATA-STORE/Playground/research/repos/logging/tests/unit_tests/factory_blog
-# Build directory: /mnt/IDMT-WORKSPACE/DATA-STORE/Playground/research/repos/logging/build/tests/unit_tests/factory_blog
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(fb_sdo_test "/mnt/IDMT-WORKSPACE/DATA-STORE/Playground/research/repos/logging/build/bin/fb_sdo_test")
-SET_TESTS_PROPERTIES(fb_sdo_test PROPERTIES  WORKING_DIRECTORY "/mnt/IDMT-WORKSPACE/DATA-STORE/Playground/research/repos/logging/build/bin")
